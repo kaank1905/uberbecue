ba3c5ea994292d5ce557f6612fa7556a13185734
RageJohnny <j.larcher1996@gmx.at>
2017-07-14 01:45:47 +0200

    Generelle CSS Überarbeitung des Files create.vue
    Neues ansprechenderes Design des Create.vue

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,18 +1,32 @@
 <template>
+
+
+
   <div class="Create">
-    <h1>{{ msg }}</h1>
+
+      <div class ="container">
+        <div class ="block">
+          <div class ="header">
+            <h1>{{ msg }}</h1>
+          </div>
+        </div>
+      </div>
+
 
 
 
 
 
     <form id="form" method="POST" action="http://localhost:3000/api/Grills"> <!-- post of form to api listening at port 3000 -->
+              {{typ}}
+      <p></p>
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-tag"></i></span>
         <input id="Bezeichnung" v-model="info.Bezeichnung" type="text" name="Bezeichnung" class="form-control" placeholder="Bezeichnung">
       </div>
       <p></p>
-
+              {{art}}
+      <p></p>
             <div class="input-group">  <!-- div container containing grill options-->
           <span class="input-group-addon"><i class="glyphicon glyphicon-fire"></i></span>
                 <select class="form-control" id="Art" name="Art">
@@ -23,7 +37,8 @@
                   </select> <!-- different option of grills to choose from -->
                   </div>
           <p></p>
-
+              {{zustand}}
+      <p></p>
             <div class="input-group">
           <span class="input-group-addon"><i class="glyphicon glyphicon-ok"></i></span>
                 <select class="form-control" id="Zustand" name="Zustand">
@@ -33,19 +48,28 @@
                   </select> <!-- different options for the grills to choose from -->
                   </div>
           <p></p>
-
+              {{ort}}
+      <p></p>
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"></i></span>
         <input id="Ort" v-model="info.Ort" type="text" name="Ort" class="form-control" placeholder="Ort">
       </div>
       <p></p>
+
+      <div class ="submit">
+
  <button type="submit" v-on:click="redirect" class="btn btn-success"><i class="glyphicon glyphicon-ok"></i> Bestätigen</button>
+      </div>
     </form>
 
+
 <p></p>
+
+    <div class ="back">
  <button type="button" class="btn btn-danger" v-on:click="redirect"><i class="glyphicon glyphicon-home"></i> Zurück zur Homepage</button>
 
  <button type="button" class="btn btn-danger" v-on:click="backHome"><i class="glyphicon glyphicon-home"></i> Zurück zur Homepage</button>
+    </div>
 
   </div>
 </template>
@@ -56,6 +80,10 @@ export default {
   data () {
     return {
       msg: 'Verleihe deinen Grill und verdiene Bares',
+      typ: 'Um welchen Typ handelt es sich?',
+      art: 'Um welche Art von Grill handelt es sich? (Bitte auswählen)',
+      zustand: 'In welchem Zustand befindet sich der Grill? (Bitte auswählen)',
+      ort: 'Wo kann man den Grill abholen?',
       info: {
         Art: '',
         Bezeichnung: '',
@@ -82,6 +110,10 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+h1 {
+  margin-bottom: 100px;
+
+}
 
 ul {
   list-style-type: none;
@@ -98,7 +130,47 @@ a {
 }
 form {
 
-margin-left: 30%;
-margin-right: 30%;
+  margin-left: 30%;
+  margin-right: 30%;
+  margin-top: 50px;
+  font-size: 20px;
 }
+  div.Create {
+
+    opacity: 1;
+    background-image: url(../assets/background.jpg);
+    color: white;
+
+  }
+  div.submit {
+    margin-top: 50px;
+  }
+  div.back {
+    margin-top: 25px;
+  }
+  div.container {
+    position: relative;
+    width: 1920px;
+    height: 200px;
+  }
+div.block {
+  background-color: #B00000;
+  filter: alpha(opacity=60);
+  /* IE */
+  -moz-opacity: 0.6;
+  /* Mozilla */
+  opacity: 0.6;
+  /* CSS3 */
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+div.header {
+    margin-top: 50px;
+}
+
+
+
 </style>
